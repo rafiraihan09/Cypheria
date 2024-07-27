0892651ed832c537953aadfa5f7f5fcf9bb27063
Louise Malvin Tanaka <louise.malvin12@gmail.com>
2024-07-27 17:59:31 +0700

    [CP-9] feat(nameplate): add driver nameplate

---
+++ frontend/components/Nameplate.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+type Props = {};
+
+const Nameplate = (props: Props) => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <div className="w-24 h-24">
+        <Avatar className="relative w-full h-full">
+          <AvatarImage
+            className="rounded-full w-full h-full object-cover"
+            src="https://github.com/shadcn.png"
+            alt="@shadcn"
+          />
+          <AvatarFallback className="rounded-full w-full h-full flex items-center justify-center">
+            CN
+          </AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="text-lg font-bold">John Doe</div>
+    </div>
+  );
+};
+
+export default Nameplate;
